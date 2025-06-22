753a0537e9122160c677bdb08cb67e40ff1b3a78
treva5 <trevamukisa5@gamil.com>
2025-06-22 12:18:13 +0300

    branch 5: first commit

--- task3/main.c
+++ task3/main.c
@@ -1,17 +1,16 @@
 #include <stdio.h>
 
 int main() {
-    float hourly_wage = 30; // UGX
-    int hours_per_week = 40;
-    int weeks_per_year = 52;
-    float annual_salary;
+    float custr1 = 200; // UGX
+    float customer2 = 500; // UGX
+    float customer3 = 300; // UGX
+    float tvenue;
 
-    // Calculate annual salary
-    annual_salary = hourly_wage * hours_per_week * weeks_per_year;
+    // Calculate total revenue
+    total_revenue = cur1 + customer2 + customer3;
 
-    // Display the result
-    printf("Annual salary: UGX %.2f\n", annual_salary);
-    system("pause");
+    // Display the total revenue
+    printf("Total revenue fl_revenue);
 
     return 0;
 }
