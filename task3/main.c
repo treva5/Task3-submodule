89a3bbee5d997a5d2ddc8aab95149c3cc3f2c8db
treva5 <trevamukisa5@gamil.com>
2025-06-22 12:11:48 +0300

    branch 4: second commit

--- task3/main.c
+++ task3/main.c
@@ -4,15 +4,14 @@ int main() {
     float hourly_wage = 30; // UGX
     int hours_per_week = 40;
     int weeks_per_year = 52;
-    float salary;
+    float annual_salary;
 
     // Calculate annual salary
-    annual_salary = hour
-
-    ly_wage * hours_per_week * weeks_per_year;
+    annual_salary = hourly_wage * hours_per_week * weeks_per_year;
 
     // Display the result
-    printf("Annual sry: UGX %.2f\n", annual_salary);
+    printf("Annual salary: UGX %.2f\n", annual_salary);
+    system("pause");
 
     return 0;
 }
