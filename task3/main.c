eaaab09e4103a58b51c11ec374977831943dac44
treva5 <trevamukisa5@gamil.com>
2025-06-22 11:22:04 +0300

    branch 1: second commit

--- task3/main.c
+++ task3/main.c
@@ -1,6 +1,5 @@
 #include <stdio.h>
 
-
 int main() {
     int total_sales;
     int days = 30;
@@ -8,13 +7,14 @@ int main() {
 
     // Prompt user to enter total sales
     printf("Enter total sales in UGX: ");
-    scanf("%d", &);
+    scanf("%d", &total_sales);
 
     // Calculate average sales with typecasting
-    averagetal_sales / days;
+    average_sales = (float)total_sales / days;
 
     // Output the result
     printf("Average sales per day: UGX %.2f\n", average_sales);
+    system("pause");
 
     return 0;
 }
