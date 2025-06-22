b66e5002b021912a77d492bb5c37edcdaf93ad8b
treva5 <trevamukisa5@gamil.com>
2025-06-22 11:37:20 +0300

    branch 2: second commit

--- task3/main.c
+++ task3/main.c
@@ -6,14 +6,15 @@ int main() {
     float commission;
 
     // Prompt the user to enter the sales volume
-    print the sales volume in UGX: ");
+    printf("Enter the sales volume in UGX: ");
     scanf("%f", &sales_volume);
 
     // Calculate the commission
-    commission =
+    commission = sales_volume * commission_rate;
 
     // Display the commission
     printf("The commission for sales of UGX %.2f at 5%% rate is: UGX %.2f\n", sales_volume, commission);
+    system("pause");
 
     return 0;
 }
