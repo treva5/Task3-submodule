2b4b108fc2b95b50af71ce50d72095edc715670c
treva5 <trevamukisa5@gamil.com>
2025-06-22 12:54:26 +0300

    initial commit

--- task3/main.c
+++ task3/main.c
@@ -1,17 +1,1 @@
-#include <stdio.h>
 
-int main() {
-    float customer1 = 200; // UGX
-    float customer2 = 500; // UGX
-    float customer3 = 300; // UGX
-    float total_revenue;
-
-    // Calculate total revenue
-    total_revenue = customer1 + customer2 + customer3;
-
-    // Display the total revenue
-    printf("Total revenue from the customers: UGX %.2f\n", total_revenue);
-    system("pause");
-
-    return 0;
-}
